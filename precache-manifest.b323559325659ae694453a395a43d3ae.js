b3e284bedbd48cb8e3d800cbd8cbfe9be5599acb
Liza Krasnoshchok <nlight1122@gmail.com>
2020-10-21 13:31:08 +0300

    Updates

---
+++ precache-manifest.b323559325659ae694453a395a43d3ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0d3caf9ebc4e5f6c8dcabd5facfc3a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5fd00d5425cf93fcf3b5",
+    "url": "./static/css/main.1939915d.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd00d5425cf93fcf3b5",
+    "url": "./static/js/main.5764fcd5.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
